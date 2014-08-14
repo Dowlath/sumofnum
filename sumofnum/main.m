4c6226c8664147276b2b2d53795b2fcf354c52b1
Dowlath <hibrise@IKRAMs-Mac-mini.local>
2014-08-14 16:52:11 +0530

    sumofnumber

--- sumofnum/main.m
+++ sumofnum/main.m
@@ -12,9 +12,30 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+//        int i,sum=0;
+//        for (i=0; i<=100; i++) {
+//            sum +=i;
+//        }
+//        NSLog(@"the sum of number is %d",sum);
+        
+        int i,odd=0,even=0;
+        for (i=0; i<=10; i++) {
+            if (i%2==0) {
+                even+=i;
+                }
+            else
+            odd +=i;
+                    }
+        NSLog(@"the odd number is %d",odd);
+        NSLog(@"the even number is %d",even);
+
+        
+        
+        
+        
         
         // insert code here...
-        NSLog(@"Hello, World!");
+        //NSLog(@"Hello, World!");
         
     }
     return 0;
